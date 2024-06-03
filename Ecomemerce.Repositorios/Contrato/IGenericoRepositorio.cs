0da8488d21a331d85420411aada6c2aae89d531c
lear23 <130910243+lear23@users.noreply.github.com>
2024-06-03 17:07:56 +0200

    Skapade en repository

---
+++ Ecomemerce.Repositorios/Contrato/IGenericoRepositorio.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Linq.Expressions;
+
+namespace Ecomemerce.Repositorios.Contrato;
+
+public interface IGenericoRepositorio<TModelo> where TModelo : class
+{
+
+    IQueryable<TModelo> Consultar(Expression<Func<TModelo, bool>>? filtro = null);
+
+    Task<TModelo> Crear(TModelo modelo);
+    Task<bool> Editar(TModelo modelo);
+    Task<bool> Eliminar(TModelo modelo);
+}
